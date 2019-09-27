e855489bfcb9a3c454c1dcaacf7a9f8ae9d488b7
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-09-27 15:38:27 -0400

    feat: upgrade to Django 2.2
    Change-Id: I456e8956f2d79f9191ae225d2fb10b3b8c72606a

--- core_module_auto_key_integer_sequence_app/urls.py
+++ core_module_auto_key_integer_sequence_app/urls.py
@@ -1,9 +1,10 @@
 """Url routing
 """
-from django.conf.urls import url
+
+from django.urls import re_path
 
 from core_module_auto_key_integer_sequence_app.views import AutoKeyIntModule
 
 urlpatterns = [
-    url(r'module-auto-key-int-sequence', AutoKeyIntModule.as_view(), name='core_auto_key_int_sequence_view'),
+    re_path(r'module-auto-key-int-sequence', AutoKeyIntModule.as_view(), name='core_auto_key_int_sequence_view'),
 ]
