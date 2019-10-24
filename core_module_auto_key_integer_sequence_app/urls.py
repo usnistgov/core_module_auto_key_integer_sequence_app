3e4733f0cb79a8241ae653f9aaa6302533a5980f
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-24 14:50:25 -0400

    fix: revert django upgrade
    Change-Id: I4bd7e54d1d96ab097a23a9424476881a1d520404

--- core_module_auto_key_integer_sequence_app/urls.py
+++ core_module_auto_key_integer_sequence_app/urls.py
@@ -1,10 +1,9 @@
 """Url routing
 """
-
-from django.urls import re_path
+from django.conf.urls import url
 
 from core_module_auto_key_integer_sequence_app.views import AutoKeyIntModule
 
 urlpatterns = [
-    re_path(r'module-auto-key-int-sequence', AutoKeyIntModule.as_view(), name='core_auto_key_int_sequence_view'),
+    url(r'module-auto-key-int-sequence', AutoKeyIntModule.as_view(), name='core_auto_key_int_sequence_view'),
 ]
