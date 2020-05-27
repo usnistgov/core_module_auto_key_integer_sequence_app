8ccbd521ea272aec9345c54c0427573a7af30ed5
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-05-27 09:53:55 -0400

    chore: apply black formatting
    Change-Id: I2ef87c5e695a3982cb9599171aa43df0bc78ca03

--- core_module_auto_key_integer_sequence_app/urls.py
+++ core_module_auto_key_integer_sequence_app/urls.py
@@ -6,5 +6,9 @@ from django.urls import re_path
 from core_module_auto_key_integer_sequence_app.views import AutoKeyIntModule
 
 urlpatterns = [
-    re_path(r'module-auto-key-int-sequence', AutoKeyIntModule.as_view(), name='core_auto_key_int_sequence_view'),
+    re_path(
+        r"module-auto-key-int-sequence",
+        AutoKeyIntModule.as_view(),
+        name="core_auto_key_int_sequence_view",
+    ),
 ]
